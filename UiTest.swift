8ba3723273985fdb6acced6c1d9d0a1a15c3323e
wlfbbz <44901408+wlfbbz@users.noreply.github.com>
2024-06-18 19:49:23 +0100

    updated caption formatting in enlarged view

---
+++ UiTest.swift
@@ -0,0 +1,18 @@
+//
+//  CongratsTest.swift
+//  WinningYear
+//
+//  Created by Barbara on 01/05/2024.
+//
+
+import SwiftUI
+
+struct UiTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UiTest()
+}
